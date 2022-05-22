790ee338a31a5f5c9d4e24e1d648769ffacfae79
JK-Chung <chungjeeken@gmail.com>
2022-05-22 12:49:19 +0100

    Changed data type of outputs to an explicit map
    * Added feature branch CICD
    
    * tomap
    
    Co-authored-by: JK-Chung <chungjeeken@outlook.com>

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "apex-domains-nameservers" {
   description = "The name servers of all apex domains. These values should be populated into the nameservers of the DNS Registrar. Intended for logging purposes."
 
-  value = {
+  value = tomap({
     for a in local.apex_domains :
     a => aws_route53_zone.apex-domains[a].name_servers
-  }
+  })
 }
 
 output "apex-domains-route53" {
   description = "Describes the Route 53 instances used for each Apex Domain. Intended for use in other Terraform workspaces"
 
-  value = {
+  value = tomap({
     for a in local.apex_domains :
     a => {
       arn     = aws_route53_zone.apex-domains[a].arn
       zone_id = aws_route53_zone.apex-domains[a].zone_id
     }
-  }
+  })
 }
