c0e5e4f0612499b813b8aa3f94517fa19dd52514
JK-Chung <chungjeeken@outlook.com>
2022-05-22 09:18:52 +0100

    Added extra terraform config

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,20 @@
-output "ds" {
-  description = "The name servers of all apex domains. These values should be populated into the nameservers of the DNS Registrar."
+output "apex-domains-nameservers" {
+  description = "The name servers of all apex domains. These values should be populated into the nameservers of the DNS Registrar. Intended for logging purposes."
 
   value = {
-    for a in local.apex_domains : 
-     a => aws_route53_zone.apex-domains[a].name_servers
+    for a in local.apex_domains :
+    a => aws_route53_zone.apex-domains[a].name_servers
+  }
+}
+
+output "apex-domains-route53" {
+  description = "Describes the Route 53 instances used for each Apex Domain. Intended for use in other Terraform workspaces"
+
+  value = {
+    for a in local.apex_domains :
+    a => {
+      arn     = aws_route53_zone.apex-domains[a].arn
+      zone_id = aws_route53_zone.apex-domains[a].zone_id
+    }
   }
 }
