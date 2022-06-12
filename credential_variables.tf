13db0ec7e878fa8704d47055f16236b73ae03ae4
JK-Chung <chungjeeken@outlook.com>
2022-06-12 19:28:05 +0100

    Added credentiald

---
+++ credential_variables.tf
@@ -0,0 +1,5 @@
+variable DEV_dns_deployer_AWS_ACCESS_KEY_ID {}
+variable DEV_dns_deployer_AWS_SECRET_ACCESS_KEY {}
+
+variable PROD_dns_deployer_AWS_ACCESS_KEY_ID {}
+variable PROD_dns_deployer_AWS_SECRET_ACCESS_KEY {}
