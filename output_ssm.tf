e71ec3df2f113fddde87ffc872f0c4300432f872
JK-Chung <chungjeeken@gmail.com>
2022-09-24 23:23:52 +0100

    Feature/SDOMAINS-4 Expose R53 Names Via SSM Parameters
    * SDOMAINS-7 Adding new SSM params
    
    * SDOMAINS-7 Changing CICD Process
    
    * SDOMAINS-7 restore CICD
    
    Co-authored-by: JK-Chung <chungjeeken@outlook.com>

--- output_ssm.tf
+++ output_ssm.tf
@@ -15,3 +15,21 @@ resource "aws_ssm_parameter" "prod_route53" {
   type        = "String"
   value       = each.value.zone_id
 }
+
+resource "aws_ssm_parameter" "dev_route53_names" {
+  provider    = aws.development
+  for_each    = aws_route53_zone.dev_hz
+  name        = format("/route53/%s/apex-domain", each.value.name)
+  description = "Describes the Route53 Apex Domain for the Hosted Zone holding records under: ${each.value.name}"
+  type        = "String"
+  value       = each.value.zone_id
+}
+
+resource "aws_ssm_parameter" "prod_route53_names" {
+  provider    = aws.PRODUCTION
+  for_each    = aws_route53_zone.prod_hz
+  name        = "/route53/${each.value.name}/apex-domain"
+  description = "Describes the Route53 Apex Domain for the Hosted Zone holding records under: ${each.value.name}"
+  type        = "String"
+  value       = each.value.zone_id
+}
